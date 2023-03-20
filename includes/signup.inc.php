b2a1b6403030f45c68eadd485842b1d497ce7041
josanua <thrueword@gmail.com>
2023-03-20 10:48:27 +0200

    Started to construct signup logic

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -1,1 +1,16 @@
 <?php
+
+if (isset($_POST['submit'])) {
+
+    // Grabbing the data
+    $uid = $_POST['uid'];
+    $pwd = $_POST['pwd'];
+    $pwdRepeat = $_POST['pwdRepeat'];
+    $email = $_POST['email'];
+
+    // Instantiate SignupContr class
+
+    // Running error handlers and user signup
+
+    // Going back to front-page
+}
